292a0601d32f5e25232612e29ab87064d70c892a
Alice Gamarnik <agamarnik1@pride.hofstra.edu>
2019-02-26 15:06:17 -0500

    Functionality to Metadata button + extractin EXIF files WIP

--- exPose/exPose/page4.swift
+++ exPose/exPose/page4.swift
@@ -23,6 +23,26 @@ class page4: UIViewController, UIImagePickerControllerDelegate, UINavigationCont
         present(imagePicker, animated: true, completion: nil)
     }
     
+    
+    @IBAction func viewMetadata(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Metadata", message: "ISO: 400; Aperture: f2.0; Shutter Speed: 1/80; ColorModel = RGB; DPIHeight = 72; DPIWidth = 72; Depth = 8; Orientation = 6; PixelHeight = 1936; PixelWidth = 2592", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+            switch action.style{
+            case .default:
+                print("default")
+                
+            case .cancel:
+                print("cancel")
+                
+            case .destructive:
+                print("destructive")
+                
+                
+            }}))
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -49,6 +69,27 @@ class page4: UIViewController, UIImagePickerControllerDelegate, UINavigationCont
             imageView.image = pickedImage
         }
         
+        dismiss(animated: true, completion: nil)
+        
+        
+        
+        //Extract Metdadata/EXIF files
+        let assetURL = info[UIImagePickerController.InfoKey.referenceURL] as! NSURL
+        let asset = PHAsset.fetchAssets(withALAssetURLs: [assetURL as URL], options: nil)
+        guard let result = asset.firstObject else {
+            return
+        }
+        
+        let imageManager = PHImageManager.default()
+        imageManager.requestImageData(for: result , options: nil, resultHandler:{
+            (data, responseString, imageOriet, info) -> Void in
+            let imageData: NSData = data! as NSData
+            if let imageSource = CGImageSourceCreateWithData(imageData, nil) {
+                let imageProperties2 = CGImageSourceCopyPropertiesAtIndex(imageSource, 0, nil)! as NSDictionary
+                print("imageProperties2: ", imageProperties2)
+            }
+            
+        })
         dismiss(animated: true, completion: nil)
     }
     
